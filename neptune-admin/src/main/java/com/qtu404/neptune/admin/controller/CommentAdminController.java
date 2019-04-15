835cae294a5e85c13ae5173b7743314a31eaeed6
wildhunt <wildhunt_geralt@foxmail.com>
2019-04-15 17:49:23 +0800

    comment enable add request body annotation

--- neptune-admin/src/main/java/com/qtu404/neptune/admin/controller/CommentAdminController.java
+++ neptune-admin/src/main/java/com/qtu404/neptune/admin/controller/CommentAdminController.java
@@ -7,6 +7,7 @@ import com.qtu404.neptune.util.model.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class CommentAdminController {
 
     @ApiOperation("评价显示/隐藏")
     @PostMapping("enable")
-    public Response<Boolean> enable(CommentEnableRequest request){
+    public Response<Boolean> enable(@RequestBody CommentEnableRequest request){
         return this.commentFacade.enableComment(request);
     }
 }
